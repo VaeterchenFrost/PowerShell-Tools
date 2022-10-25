1088312c87d71d9ad57f2e903999f4295bbe4262
Martin Röbke <martin.roebke@web.de>
2022-10-25 12:02:11 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,16 @@
-Set-Alias cl clear                                                                                                      
-function whoareyou() {Get-ComputerInfo | select CsName,CsManufacturer,CsModel,Osname,osbuildnumber}                     
-function battery() {(Get-CimInstance win32_battery).EstimatedChargeRemaining} 
+Set-Alias cl clear   
+Set-Alias top Taskmgr.exe                                                                                                   
+function whoareyou() { Get-ComputerInfo | Select-Object CsName, CsManufacturer, CsModel, Osname, osbuildnumber }                     
+function battery()
+{
+ (Get-CimInstance win32_battery).EstimatedChargeRemaining;
+   if ((Get-CimInstance Win32_Battery).BatteryStatus -eq 2)
+   {
+      Write-Host 'Loading'
+   };
+}
+function clock() { while ($true) { Write-Progress -Activity 'PowerShell Clock'  -Status (Get-Date -Format F) -PercentComplete ((Get-Date).Second / .6); Start-Sleep -Milliseconds 99 } }
+
 # Shell:                                                                                                                
 $wscript = New-Object -ComObject WScript.Shell
 
@@ -8,25 +18,25 @@ Import-Module assert
 (Split-Path -Path (Get-Module assert).path ) + '\Format\src\Format.psm1' | Import-Module
 New-Alias nice Format-Collection
 
-function node-project {
-    git init
-    npx license $(npm get init.license) -o "$(npm get init.author.name)" > LICENSE
-    npx gitignore node
-    npx covgen "$(npm get init.author.email)"
-    npm init -y
-    git add -A
-    git commit -m 'Initial commit'
+function node-project
+{
+   git init
+   npx license $(npm get init.license) -o "$(npm get init.author.name)" > LICENSE
+   npx gitignore node
+   npx covgen "$(npm get init.author.email)"
+   npm init -y
+   git add -A
+   git commit -m 'Initial commit'
 }
 
-function new-pw ($length) {
-   -join ((1..$length) | % {Get-Random -Min 33 -Max 127 | %{[char]$_}}) | set-clipboard
+function new-pw ($length)
+{
+   -join ((1..$length) | ForEach-Object { Get-Random -Min 33 -Max 127 | ForEach-Object { [char]$_ } }) | Set-Clipboard
 }
 
-function leelatraingpu {
-   Start-Process powershell -ArgumentList nvidia-smi.exe,dmon ;
-   cd C:\Users\Martin\lc0-v0.29.0-rc\;
-   &"./lc0-training-client.exe" --password $env:LC0_PW --user $env:LC0_USER --report-gpu;
-   cd -;
+function conda
+{
+   powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\ProgramData\Anaconda3' "    
 }
 
 $equal_tempered_scale = @{Eb2 = 77.78; E2 = 82.41; F2 = 87.31; Gb2 = 92.50; G2 = 98.00; Ab2 = 103.83; A2 = 110.00; Bb2 = 116.54; B2 = 123.47; C3 = 130.81; Db3 = 138.59; D3 = 146.83; Eb3 = 155.56; E3 = 164.81; F3 = 174.61; Gb3 = 185.00; G3 = 196.00; Ab3 = 207.65; A3 = 220.00; Bb3 = 233.08; B3 = 246.94; C4 = 261.63; Db4 = 277.18; D4 = 293.66; Eb4 = 311.13; E4 = 329.63; F4 = 349.23; Gb4 = 369.99; G4 = 392.00; Ab4 = 415.30; A4 = 440.00; Bb4 = 466.16; B4 = 493.88; C5 = 523.25; Db5 = 554.37; D5 = 587.33; Eb5 = 622.25; E5 = 659.25; F5 = 698.46; Gb5 = 739.99; G5 = 783.99; Ab5 = 830.61; A5 = 880.00; Bb5 = 932.33; B5 = 987.77; C6 = 1046.50; Db6 = 1108.73; D6 = 1174.66; Eb6 = 1244.51; E6 = 1318.51; F6 = 1396.91; Gb6 = 1479.98; G6 = 1567.98; Ab6 = 1661.22; A6 = 1760.00; Bb6 = 1864.66; B6 = 1975.53 }
@@ -60,3 +70,14 @@ namespace Utilities {
    }
 }
 '
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile))
+{
+   Import-Module "$ChocolateyProfile"
+}
